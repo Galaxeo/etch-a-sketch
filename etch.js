7ed5c85176b8ed8f1fe4bcd45c99ddb499101e65
Galaxeo <hwjustincheok@gmail.com>
2023-04-30 13:17:15 -0400

    feat: add mouseover functionality

--- etch.js
+++ etch.js
@@ -3,14 +3,14 @@ function genDivs() {
   for (let i = 0; i < 4; i++) {
     let row = document.createElement('div');
     row.className = 'row';
-    for (let j = 0; j <= 4; j++) {
+    for (let j = 0; j < 4; j++) {
       let cell = document.createElement('div');
       cell.className = 'gridSquare';
-      cell.innerText = (i * 4) + j;
       row.appendChild(cell);
     }
     e.appendChild(row);
   }
-  document.getElementById("code").innerText = e.innerHTML;
 }
 genDivs();
+const tiles = Array.from(document.querySelectorAll('.gridSquare'));
+tiles.forEach(tile => tile.addEventListener('mouseover', function() { tile.style.background = "black"; }));
